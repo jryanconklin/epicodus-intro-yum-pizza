26d49df3caef3788e54e09c9f7adb03537a00bb8
J. Ryan Conklin <jryanconklin@gmail.com>
2016-07-14 15:00:22 -0700

    Remove Toppings Push Prototype and Set Toppings Variable by jQuery Checkbox Input.

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,36 @@
 // Business Logic
 
-function Pizza(size, base) {
+function Pizza(size, base, toppings) {
   this.size = size;
   this.base = base;
-  this.toppings = [];
+  this.toppings = toppings;
   this.price;
 }
 
-Pizza.prototype.addTopping = function(topping) {
-  this.toppings.push(topping);
-}
-
-Pizza.prototype.calcToppingPrice = function() {
-  this.toppings.forEach(topping) {
-    this.price += 2;
-  }
+Pizza.prototype.calcPrice = function() {
+  
+    if (this.size === "Small") {
+      this.price += 10;
+    } else if (this.size === "Medium") {
+      this.price += 15;
+    } else if (this.size === "Large") {
+      this.price += 20;
+    }
+  return this.price;
 }
 
 // User Interface Logic
 $(document).ready(function() {
   $("form#pizza").submit(function(event) {
     event.preventDefault();
+    var size = $("input:radio[name=size]:checked").val();
+    var base = $("input:radio[name=base]:checked").val();
+    var toppings = $("input:checkbox[name=toppings]:checked").map(function(){
+      return $(this).val();
+    }).get();
+    var userPizza = new Pizza(size, base, toppings);
 
-    var userPizza = new Pizza();
-
-
-
-
+    userPizza.price = userPizza.calcPrice();
+    alert(userPizza.price);
   });
 });
